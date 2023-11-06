eb2f17b162aea0c1bc8482727c51b7f7cec887a8
shivraj <sk40fs@gmail.com>
2023-11-06 23:07:20 +0530

    'apk-done'

--- src/landing.jsx
+++ src/landing.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react'
-
+import React from 'react'
+// import apkFile from "./assets/app-debug.apk";
 import d from "./w.png"
 const landing = () => {
 
-
+    // const handleDownload = () => {
+    //     const link = document.createElement("a");
+    //     link.href = apkFile;
+    //     link.download = "app-debug.apk";
+    //     link.click();
+    // };
     return (
         <div>
             <div className='flex flex-col sm:flex-row'>
@@ -14,7 +19,11 @@ const landing = () => {
                 <div className='w-[100%] sm:w-[50%] flex flex-col justify-center items-center'>
                     <p className='font-bold text-xl sm:text-5xl text-green-600'>Welcome to Java Tutorial</p>
                     <p className='font-bold text-lg sm:text-4xl mt-4 text-green-600'>Learn Java With Us !!</p>
-                    <a href='app-debug.apk'><button className='px-5 py-1 text-white font-bold bg-green-700 rounded-md mt-10'>Download App</button></a>
+                    <a
+                        href="app-debug.apk"
+                        download="app-debug.apk"
+                    ><button  className='px-5 py-1 text-white font-bold bg-green-700 rounded-md mt-10'>Download App</button>
+                    </a>
                 </div>
 
             </div>
